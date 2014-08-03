34d53e46599ba6fd845835c4147ea4aea8142476
bede <ngarukob@yahoo.com>
2014-08-04 02:47:13 +1200

    Intitial

---
+++ ParameterEditor/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using ParameterEditor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+//using ParameterEditor.Pagination;
+using PagedList;
+
+namespace ParameterEditor.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
